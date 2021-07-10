7492d357fcd44d86d90283e29c2472d4ff444553
jessie <374649328@qq.com>
2021-07-10 16:40:39 +0800

    ADD

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print("hello worlf")
+import numpy as np
+
+output = []
+a = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
+b = np.array(a)
+
+# 换列
+# b[:, [0, 2]] = b[:, [2, 0]]
+# b[:, [1, 3]] = b[:, [3, 1]]
+# print(b)
+
+
+arr = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12]])
+print(arr)
+arr = np.delete(arr, 0, axis=1)
+print(arr)
